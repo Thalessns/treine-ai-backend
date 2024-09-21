32feb96a9227136755c9ed42322ea1c677e8181e
Thales <tthales08@gmail.com>
2024-09-21 15:56:42 -0300

    Adicionando rotas de usuário

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 
 from src.llm.router import llm_router
+from src.database.user.router import user_router
+from src.database.utils import Database
 
 app = FastAPI(
     title="Treine.AI - API",
     version="0.1.0"
 )
 
+app.add_event_handler("startup", Database.init_models)
 app.include_router(llm_router)
+app.include_router(user_router)
